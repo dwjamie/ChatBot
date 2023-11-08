9e8b9f6fd85512d008c440059afaa8af88cc4a7b
Jamie Cao <jc2974@cornell.edu>
2023-11-07 19:00:00 -0500

    Update OpenAI models

--- utils/llm.py
+++ utils/llm.py
@@ -6,13 +6,13 @@ def create_chatbot(model, temperature, system_message, functions=None, pl_tags=[
     if model == "GPT-3.5":
         st.session_state.chatbot = agent.OpenAI(
             functions=functions,
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo-1106",
             temperature=temperature,
             pl_tags=pl_tags,
         )
     elif model == "GPT-4":
         st.session_state.chatbot = agent.OpenAI(
-            functions=functions, model="gpt-4", temperature=temperature, pl_tags=pl_tags
+            functions=functions, model="gpt-4-1106-preview", temperature=temperature, pl_tags=pl_tags
         )
     elif model == "Claude 1":
         st.session_state.chatbot = agent.Claude(
