4be8b7012be64e98b6fb4834234b574f64e85190
Jamie Cao <jc2974@cornell.edu>
2024-03-05 13:41:01 -0500

    update

--- agent/chatbot.py
+++ agent/chatbot.py
@@ -6,7 +6,7 @@ import promptlayer
 promptlayer.api_key = st.secrets["promptlayer"]["api_key"]
 
 openai = promptlayer.openai
-os.environ["OPENAI_API_KEY"] = st.secrets["anthropic"]["api_key"]
+os.environ["OPENAI_API_KEY"] = st.secrets["openai"]["api_key"]
 
 anthropic = promptlayer.anthropic
 os.environ["ANTHROPIC_API_KEY"] = st.secrets["anthropic"]["api_key"]
@@ -25,14 +25,17 @@ class OpenAI:
     def chat_no_stream(self, user_message):
         self.messages.append({"role": "user", "content": user_message})
         chatgpt = openai.OpenAI()
-        assistant_message = chatgpt.chat.completions.create(
-            messages=self.messages,
-            model=self.model,
-            temperature=self.temperature,
-            pl_tags=self.pl_tags,
-        )["choices"][0]["message"]
-        self.messages.append(assistant_message)
-        return assistant_message["content"]
+        assistant_message = (
+            chatgpt.chat.completions.create(
+                messages=self.messages,
+                model=self.model,
+                temperature=self.temperature,
+                pl_tags=self.pl_tags,
+            )
+            .choices[0]
+            .message
+        )
+        return assistant_message.content
 
     def chat(self, user_message):
         self.messages.append({"role": "user", "content": user_message})
@@ -53,40 +56,49 @@ class OpenAI:
 class Claude:
     def __init__(self, **kwargs):
         anthropic.api_key = st.secrets["anthropic"]["api_key"]
-        self.messages = kwargs.get("messages", "")
+        self.messages = kwargs.get("messages", [])
+        self.system = ""
         self.model = kwargs.get("model", "claude-2.1")
         self.temperature = kwargs.get("temperature", None)
         self.pl_tags = kwargs.get("pl_tags", [])
 
     def add_message(self, role, content):
-        if role == "assistant":
-            self.messages += "\n\nAssistant:" + " " + content
-        else:
-            self.messages += "\n\nHuman:" + " " + content
+        if role in ["user", "assistant"]:
+            if self.messages[-1]["role"] == role:
+                self.messages[-1]["content"] += "\n" + content
+            else:
+                self.messages.append({"role": role, "content": content})
+        elif role == "system":
+            self.system = content
 
     def chat_no_stream(self, user_message):
-        self.messages += "\n\nHuman:" + " " + user_message + "\n\nAssistant:" + " "
+        self.messages.append({"role": "user", "content": user_message})
         claude = anthropic.Anthropic()
-        assistant_message = claude.completions.create(
-            prompt=self.messages,
-            model=self.model,
-            temperature=self.temperature,
-            max_tokens_to_sample=2048,
-            pl_tags=self.pl_tags,
-        ).completion
-        self.messages += assistant_message
+        assistant_message = (
+            claude.messages.create(
+                messages=self.messages,
+                system=self.system,
+                model=self.model,
+                temperature=self.temperature,
+                max_tokens=2048,
+                pl_tags=self.pl_tags,
+            )
+            .content[0]
+            .text
+        )
         return assistant_message
 
     def chat(self, user_message):
-        self.messages += "\n\nHuman:" + " " + user_message + "\n\nAssistant:" + " "
+        self.messages.append({"role": "user", "content": user_message})
         claude = anthropic.Anthropic()
-        assistant_response = claude.completions.create(
-            prompt=self.messages,
+        with claude.messages.stream(
+            messages=self.messages,
+            system=self.system,
             model=self.model,
             temperature=self.temperature,
-            max_tokens_to_sample=2048,
-            stream=True,
+            max_tokens=2048,
             pl_tags=self.pl_tags,
-        )
-        for chunk in assistant_response:
-            yield chunk.completion
+        ) as assistant_response:
+            for token in assistant_response.text_stream:
+                if token:
+                    yield token
